3b86296deafaa13cfb5c086409bd4e3ea04fbfb3
Shin Jiwon <albenyu12@gmail.com>
2025-12-02 22:27:52 +0900

    [학습] 자료구조

---
+++ 2_NODE/3_DS/2_stack.js
@@ -0,0 +1,43 @@
+class Stack {
+    constructor() {
+        this.stack = {};
+        this.count = 0;
+    }
+
+    push(element) {
+        this.stack[this.count] = element;
+        this.count++;
+    }
+
+    pop() {
+        if (this.count == 0) {
+            return "가져올게 아무것도 없음";
+        }
+
+        this.count--;
+        const result = this.stack[this.count];
+        return result;
+    }
+
+    size() {
+        return this.count;
+    }
+}
+
+const myStack = new Stack();
+console.log(myStack.size());
+
+myStack.push("초록색");
+myStack.push("노란색");
+myStack.push("주황색");
+myStack.push("파란색");
+myStack.push("빨간색");
+
+console.log(myStack.size());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
