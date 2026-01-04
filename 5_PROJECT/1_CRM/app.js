30cf0eff7a4b55732b9bdbd0b7d0531be82f1ba9
Shin Jiwon <albenyu12@gmail.com>
2026-01-04 15:37:08 +0900

    [CRM] 성별 검색

--- 5_PROJECT/1_CRM/app.js
+++ 5_PROJECT/1_CRM/app.js
@@ -9,22 +9,32 @@ const db = new Database(path.join(__dirname, 'user-sample.db'));
 app.use(express.json());
 app.use(express.static('public'));
 
-// curl -X GET "127.0.0.1:3000/api/crm/users?name=%EA%B9%80&page=3&size=10"
+// curl -X GET "127.0.0.1:3000/api/crm/users?name=%EA%B9%80&gender=Male&page=3&size=10"
 app.get('/api/crm/users', (req, res) => {
     const page = parseInt(req.query.page);
     const size = parseInt(req.query.size);
     const name = req.query.name;
+    const gender = req.query.gender;
     const offset = (page - 1) * size;
     let sql = `SELECT * FROM users`
     let params = [];
 
-    if (name) {
-        sql += ` WHERE Name LIKE ?`;
-        params.push(`%${name}%`);
-    }
+    // if (!name) {
+    //     return;
+    // } else {
+    //     sql += ` WHERE Name LIKE ?`;
+    //     params.push(`%${name}%`);
+    // }
+
+    // if (!gender) {
+    //     sql += ` AND Gender = ?`;
+    //     params.push(`${gender}`);
+    // }
 
     sql += ` ORDER BY Id LIMIT ? OFFSET ?`;
     params.push(size, offset);
+    console.log(sql);
+
     try {
         console.log(params);
 
